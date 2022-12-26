aae0a4e89bc8e4d7106224786cebe623c2726614
Orlan Quijada <orlantquijada@gmail.com>
2022-12-27 03:12:59 +0800

    feat: typescript alias

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,7 +1,18 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            "~": "./src",
+          },
+        },
+      ],
+    ],
     presets: ["babel-preset-expo"],
-  };
-};
+  }
+}
