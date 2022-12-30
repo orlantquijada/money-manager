ee4347c81df3c0e50b3f625520a1e68fc53b3e3c
Orlan Quijada <orlantquijada@gmail.com>
2022-12-31 04:12:57 +0800

    chore: tamagui babel config

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -13,6 +13,21 @@ module.exports = function (api) {
           },
         },
       ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./src/tamagui.config.ts",
+          // logTimings: true,
+          // disableExtraction: process.env.NODE_ENV === "development",
+        },
+      ],
+      [
+        "transform-inline-environment-variables",
+        {
+          include: "TAMAGUI_TARGET",
+        },
+      ],
       "react-native-reanimated/plugin",
     ],
     presets: ["babel-preset-expo"],
