265566cb5d739dcf1cdc742e2bd96bd272ab04e2
Orlan Quijada <orlantquijada@gmail.com>
2022-12-30 21:05:16 +0800

    wip: schema

--- packages/api/src/router/transactions.ts
+++ packages/api/src/router/transactions.ts
@@ -1,12 +1,13 @@
 import { router, publicProcedure } from "../trpc"
 import { z } from "zod"
+import { prisma } from "db"
 
-export const postRouter = router({
+export const transactionsRouter = router({
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany()
+    return ctx.prisma.transaction.findMany()
   }),
   retrieve: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.post.findFirst({ where: { id: input } })
+    return ctx.prisma.transaction.findFirst({ where: { id: input } })
   }),
   create: publicProcedure
     .input(z.object({ title: z.string(), content: z.string() }))
