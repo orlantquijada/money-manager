2934660528e5abc3fff6a4c187e116ad3fb6eb0d
Orlan Quijada <orlantquijada@gmail.com>
2023-01-27 23:07:07 +0800

    feat: useToggle

---
+++ apps/expo/src/utils/hooks/useToggle.ts
@@ -0,0 +1,21 @@
+import { useCallback, useReducer } from "react"
+
+type UseToggleProps = {
+  initial?: boolean
+}
+
+export default function useToggle(props: UseToggleProps = {}) {
+  const { initial = false } = props
+
+  const [state, handleToggle] = useReducer(
+    (prev: boolean, next: boolean) => (next == null ? !prev : next),
+    initial,
+  )
+
+  const on = useCallback(() => handleToggle(true), [])
+  const off = useCallback(() => handleToggle(false), [])
+  // bypass typescript
+  const toggle = useCallback(() => handleToggle(undefined as any), [])
+
+  return [state, { on, off, toggle }] as const
+}
