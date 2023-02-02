fc8d1242422fa2a86cd4620bfca911f3da996cfa
Orlan Quijada <orlantquijada@gmail.com>
2023-02-02 22:43:01 +0800

    feat: transitions on disabled

--- apps/expo/src/components/Button.tsx
+++ apps/expo/src/components/Button.tsx
@@ -1,21 +1,32 @@
+import { type ComponentProps } from "react"
 import clsx from "clsx"
-import { ComponentProps } from "react"
-import { View } from "react-native"
+import { MotiView } from "moti"
+import { styled } from "nativewind"
 
+import { mauveDark } from "~/utils/colors"
+
+const StyledMotiView = styled(MotiView)
 type Props = {
   disabled?: boolean
-} & ComponentProps<typeof View>
+} & ComponentProps<typeof StyledMotiView>
 
 // only a UI comp does not actually do what a button does
 // wrap this with a <Pressable /> to have button functionality
 export default function Button({ className, disabled, ...rest }: Props) {
   return (
-    <View
+    <StyledMotiView
       className={clsx(
-        "bg-mauveDark12 h-8 justify-center rounded-xl px-4",
-        disabled && "bg-mauveDark11",
+        "h-8 justify-center rounded-xl px-4 transition-colors",
         className,
       )}
+      animate={{
+        backgroundColor: disabled ? mauveDark.mauve11 : mauveDark.mauve12,
+        ...rest.animate,
+      }}
+      transition={{
+        backgroundColor: { type: "timing", duration: 200 },
+        ...rest.transition,
+      }}
       {...rest}
     />
   )
