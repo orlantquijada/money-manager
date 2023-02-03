adf04d5e74b1a5a761d61c8057ede781fa7cffb2
Orlan Quijada <orlantquijada@gmail.com>
2023-02-04 02:26:14 +0800

    feat: more animations stuff

--- apps/expo/src/components/create-fund/FundInfo.tsx
+++ apps/expo/src/components/create-fund/FundInfo.tsx
@@ -1,17 +1,48 @@
-import { ComponentProps, FC } from "react"
+import { ComponentProps, FC, useState } from "react"
 import { View, Text, ScrollView } from "react-native"
 import { SvgProps } from "react-native-svg"
 import clsx from "clsx"
 
+import { transitions } from "~/utils/motion"
+
 import Presence from "~/components/Presence"
 import TextInput from "~/components/TextInput"
 import CreateFooter from "~/components/CreateFooter"
+
+import ScaleDownPressable from "../ScaleDownPressable"
+import StyledMotiView from "../StyledMotiView"
+import { type Fund } from ".prisma/client"
 
 import ShoppingBagIcon from "../../../assets/icons/shopping-bag.svg"
 import LockIcon from "../../../assets/icons/lock.svg"
 import GPSIcon from "../../../assets/icons/gps.svg"
 
 const DELAY = 40
+const FUND_CARD_HEIGHT = 65
+const FUND_CARD_GAP = 8
+
+const translateY: Record<Fund["fundType"], number> = {
+  SPENDING: 0,
+  NON_NEGOTIABLE: FUND_CARD_GAP + FUND_CARD_HEIGHT,
+  TARGET: (FUND_CARD_GAP + FUND_CARD_HEIGHT) * 2,
+}
+const presenceProps = {
+  SPENDING: {
+    delayMultiplier: 7,
+    delay: DELAY,
+    exitDelayMultiplier: 3,
+  },
+  NON_NEGOTIABLE: {
+    delayMultiplier: 8,
+    delay: DELAY,
+    exitDelayMultiplier: 4,
+  },
+  TARGET: {
+    delayMultiplier: 9,
+    delay: DELAY,
+    exitDelayMultiplier: 5,
+  },
+} as const
 
 export default function FundInfo({
   onPress,
@@ -20,6 +51,11 @@ export default function FundInfo({
   onPress: () => void
   onBackPress: () => void
 }) {
+  const [selectedType, setSelectedType] = useState<Fund["fundType"]>("SPENDING")
+  const [fundName, setFundName] = useState("")
+
+  const disabled = !fundName || !selectedType
+
   return (
     <>
       <ScrollView
@@ -32,7 +68,12 @@ export default function FundInfo({
               <Text className="text-mauveDark12 font-satoshi-medium text-lg">
                 What's the name of your fund?
               </Text>
-              <TextInput placeholder="new-fund" />
+              <TextInput
+                placeholder="new-fund"
+                value={fundName}
+                onChangeText={setFundName}
+                autoFocus
+              />
             </View>
           </Presence>
 
@@ -43,55 +84,64 @@ export default function FundInfo({
               </Text>
             </Presence>
 
-            <View>
-              <Presence
-                delayMultiplier={7}
-                delay={DELAY}
-                exitDelayMultiplier={3}
-              >
-                <FundCard
-                  Icon={ShoppingBagIcon}
-                  label="For Spending"
-                  description="Description"
-                  selected
-                  className="mb-2"
+            <View className="relative">
+              <Presence {...presenceProps[selectedType]}>
+                <StyledMotiView
+                  className="bg-mauveDark4 absolute top-0 left-0 right-0 h-[65px] rounded-xl"
+                  animate={{ translateY: translateY[selectedType] }}
+                  transition={transitions.snappy}
                 />
               </Presence>
-
-              <Presence
-                delayMultiplier={8}
-                delay={DELAY}
-                exitDelayMultiplier={4}
-              >
-                <FundCard
-                  Icon={LockIcon}
-                  label="Non-negotiables"
-                  description="Description"
-                />
+              <Presence {...presenceProps["SPENDING"]}>
+                <ScaleDownPressable onPress={() => setSelectedType("SPENDING")}>
+                  <FundCard
+                    Icon={ShoppingBagIcon}
+                    label="For Spending"
+                    description="Description"
+                    selected={selectedType === "SPENDING"}
+                    className="mb-2"
+                  />
+                </ScaleDownPressable>
               </Presence>
 
-              <Presence
-                delayMultiplier={9}
-                delay={DELAY}
-                exitDelayMultiplier={5}
-              >
-                <FundCard
-                  Icon={GPSIcon}
-                  label="Targets"
-                  description="Description"
-                />
+              <Presence {...presenceProps["NON_NEGOTIABLE"]}>
+                <ScaleDownPressable
+                  onPress={() => setSelectedType("NON_NEGOTIABLE")}
+                >
+                  <FundCard
+                    Icon={LockIcon}
+                    label="Non-negotiables"
+                    description="Description"
+                    selected={selectedType === "NON_NEGOTIABLE"}
+                    className="mb-2"
+                  />
+                </ScaleDownPressable>
+              </Presence>
+
+              <Presence {...presenceProps["TARGET"]}>
+                <ScaleDownPressable onPress={() => setSelectedType("TARGET")}>
+                  <FundCard
+                    Icon={GPSIcon}
+                    label="Targets"
+                    description="Description"
+                    selected={selectedType === "TARGET"}
+                  />
+                </ScaleDownPressable>
               </Presence>
             </View>
           </View>
         </View>
       </ScrollView>
-      <CreateFooter onContinuePress={onPress} onBackPress={onBackPress} />
+      <CreateFooter
+        disabled={disabled}
+        onContinuePress={onPress}
+        onBackPress={onBackPress}
+      />
     </>
   )
 }
 
 function FundCard({
-  selected = false,
   label,
   description,
   Icon,
@@ -106,12 +156,8 @@ function FundCard({
   style?: ComponentProps<typeof View>["style"]
 }) {
   return (
-    <View
-      className={clsx(
-        selected && "bg-mauveDark4",
-        "flex flex-row rounded-xl py-3 px-4",
-        className,
-      )}
+    <StyledMotiView
+      className={clsx("flex flex-row rounded-xl py-3 px-4", className)}
       style={style}
     >
       <View className="mt-[6px] mr-4">
@@ -126,6 +172,6 @@ function FundCard({
           {description}
         </Text>
       </View>
-    </View>
+    </StyledMotiView>
   )
 }
