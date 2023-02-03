3009eee516f64c2cdb5ecd1af78194d18542570b
Orlan Quijada <orlantquijada@gmail.com>
2023-02-03 23:51:02 +0800

    feat: folders api

---
+++ packages/api/src/router/folders.ts
@@ -0,0 +1,20 @@
+import { z } from "zod"
+import { publicProcedure, router } from "../trpc"
+
+export const foldersRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        userId: z.string(),
+      }),
+    )
+    .mutation(({ input, ctx }) => ctx.prisma.folder.create({ data: input })),
+  listWithFunds: publicProcedure.query(({ ctx }) =>
+    ctx.prisma.folder.findMany({
+      include: {
+        funds: true,
+      },
+    }),
+  ),
+})
--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -2,11 +2,13 @@ import { router } from "../trpc"
 import { postRouter } from "./post"
 import { authRouter } from "./auth"
 import { transactionsRouter } from "./transactions"
+import { foldersRouter } from "./folders"
 
 export const appRouter = router({
   post: postRouter,
   auth: authRouter,
   transaction: transactionsRouter,
+  folder: foldersRouter,
 })
 
 // export type definition of API
