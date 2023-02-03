d4adb33761c3a32ab60b82c8568fbfd417a9e677
Orlan Quijada <orlantquijada@gmail.com>
2023-02-03 22:11:24 +0800

    feat: added default loading prop on Button

--- apps/expo/src/components/Button.tsx
+++ apps/expo/src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { type ComponentProps } from "react"
+import { View, ActivityIndicator } from "react-native"
 import clsx from "clsx"
 import { MotiView } from "moti"
 import { styled } from "nativewind"
@@ -8,15 +9,25 @@ import { mauveDark } from "~/utils/colors"
 const StyledMotiView = styled(MotiView)
 type Props = {
   disabled?: boolean
+  loading?: boolean
+  loadingColor?: string
 } & ComponentProps<typeof StyledMotiView>
 
 // only a UI comp does not actually do what a button does
 // wrap this with a <Pressable /> to have button functionality
-export default function Button({ className, disabled, ...rest }: Props) {
+export default function Button(props: Props) {
+  const {
+    className,
+    children,
+    disabled = false,
+    loading = false,
+    loadingColor = mauveDark.mauve1,
+    ...rest
+  } = props
   return (
     <StyledMotiView
       className={clsx(
-        "h-8 justify-center rounded-xl px-4 transition-colors",
+        "relative h-8 items-center justify-center rounded-xl px-4 transition-colors",
         className,
       )}
       animate={{
@@ -28,6 +39,15 @@ export default function Button({ className, disabled, ...rest }: Props) {
         ...rest.transition,
       }}
       {...rest}
-    />
+    >
+      <View className={clsx(loading && "opacity-0")}>{children}</View>
+      {loading && (
+        <ActivityIndicator
+          className="absolute"
+          size="small"
+          color={loadingColor}
+        />
+      )}
+    </StyledMotiView>
   )
 }
