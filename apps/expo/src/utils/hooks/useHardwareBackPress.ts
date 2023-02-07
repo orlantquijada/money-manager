767a4e8fc3f9261e5815bdaa4eeb95c8ba3cd5e6
Orlan Quijada <orlantquijada@gmail.com>
2023-02-07 16:07:29 +0800

    feat: useHardwareBackPress hook

---
+++ apps/expo/src/utils/hooks/useHardwareBackPress.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { BackHandler } from "react-native"
+
+export const useHardwareBackPress = (callback: () => void) => {
+  useEffect(() => {
+    const handleOnBackPress = () => {
+      callback()
+      return true
+    }
+
+    BackHandler.addEventListener("hardwareBackPress", handleOnBackPress)
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleOnBackPress)
+    }
+  }, [callback])
+}
