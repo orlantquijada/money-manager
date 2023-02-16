98f58d4cae8b35b197b67f387ae98cff110eb980
Orlan Quijada <orlantquijada@gmail.com>
2023-02-16 19:14:52 +0800

    Update functions.ts to add range and clamp functions.

--- apps/expo/src/utils/functions.ts
+++ apps/expo/src/utils/functions.ts
@@ -1,3 +1,12 @@
 export const getRandomChoice = <T>(arr: T[]) => {
   return arr[Math.floor(Math.random() * arr.length)] as T
 }
+
+export const range = (size: number) => {
+  return [...Array(size).keys()]
+}
+
+export const clamp = (num: number, min: number, max: number) => {
+  "worklet"
+  return Math.min(Math.max(num, min), max)
+}
