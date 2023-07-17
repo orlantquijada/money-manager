c60ca6a25e9f60d8c138d0bffdb2922f613dd50b
Orlan Quijada <orlantquijada@gmail.com>
2023-07-17 16:54:11 +0800

    chore: migrate to expoconfig host uri

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -16,7 +16,9 @@ const getBaseUrl = () => {
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
    */
-  const localhost = Constants.manifest?.debuggerHost?.split(":")[0]
+  const hostUri =
+    Constants.expoConfig?.hostUri ?? Constants.expoGoConfig?.debuggerHost
+  const localhost = hostUri?.split(":")[0]
   console.log({ localhost })
   if (!localhost)
     throw new Error("failed to get localhost, configure it manually")
