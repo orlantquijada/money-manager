66535cd75a3a39b4f700496131c83a1089731139
Orlan Quijada <orlantquijada@gmail.com>
2023-07-18 19:20:03 +0800

    fix: unused defaultOpen prop

--- apps/expo/src/components/AnimateHeight.tsx
+++ apps/expo/src/components/AnimateHeight.tsx
@@ -1,6 +1,5 @@
 import { ComponentProps, PropsWithChildren } from "react"
 import { View } from "react-native"
-import { SharedValue } from "react-native-reanimated"
 import { MotiView } from "moti"
 import { styled } from "nativewind"
 import clsx from "clsx"
@@ -8,23 +7,22 @@ import clsx from "clsx"
 import { transitions } from "~/utils/motion"
 import {
   useAnimateHeight,
+  UseAnimateProps,
   useMeasureHeight,
 } from "~/utils/hooks/useAnimateHeight"
 
 type Props = PropsWithChildren<
-  {
-    open: SharedValue<boolean>
-  } & ComponentProps<typeof MotiView>
+  UseAnimateProps & ComponentProps<typeof MotiView>
 >
 
 const StyledMotiView = styled(MotiView)
 const INITIAL_HEIGHT = 0
 
 export function AnimateHeight(props: Props) {
-  const { open, style, className, children, ...rest } = props
+  const { open, defaultOpen, style, className, children, ...rest } = props
 
   const { measuredHeight, handleOnLayout } = useMeasureHeight(INITIAL_HEIGHT)
-  const { animate } = useAnimateHeight(measuredHeight, { open })
+  const { animate } = useAnimateHeight(measuredHeight, { open, defaultOpen })
 
   return (
     <StyledMotiView
