cb3fcb3aaf02cfb6cfd4ea60081d33576747fb0e
Orlan Quijada <orlantquijada@gmail.com>
2023-07-19 18:39:55 +0800

    chore: replace nunito with local copy

--- apps/expo/src/utils/hooks/useFonts.ts
+++ apps/expo/src/utils/hooks/useFonts.ts
@@ -1,14 +1,14 @@
 import { useFonts as useExpoFonts } from "expo-font"
-import {
-  Nunito_200ExtraLight,
-  Nunito_300Light,
-  Nunito_400Regular,
-  Nunito_500Medium,
-  Nunito_600SemiBold,
-  Nunito_700Bold,
-  Nunito_800ExtraBold,
-  Nunito_900Black,
-} from "@expo-google-fonts/nunito"
+// import {
+//   Nunito_200ExtraLight,
+//   Nunito_300Light,
+//   Nunito_400Regular,
+//   Nunito_500Medium,
+//   Nunito_600SemiBold,
+//   Nunito_700Bold,
+//   Nunito_800ExtraBold,
+//   Nunito_900Black,
+// } from "@expo-google-fonts/nunito"
 
 export function useFonts() {
   const [loaded] = useExpoFonts({
@@ -19,14 +19,11 @@ export function useFonts() {
     "Satoshi-Bold": require("../../../assets/fonts/Satoshi/Satoshi-Bold.ttf"),
     "Satoshi-BoldItalic": require("../../../assets/fonts/Satoshi/Satoshi-BoldItalic.ttf"),
     "AzeretMono-Regular": require("../../../assets/fonts/AzeretMono/AzeretMono-Regular.ttf"),
-    Nunito_200ExtraLight,
-    Nunito_300Light,
-    Nunito_400Regular,
-    Nunito_500Medium,
-    Nunito_600SemiBold,
-    Nunito_700Bold,
-    Nunito_800ExtraBold,
-    Nunito_900Black,
+    Nunito_400Regular: require("../../../assets/fonts/Nunito/Nunito-Regular.ttf"),
+    Nunito_500Medium: require("../../../assets/fonts/Nunito/Nunito-Medium.ttf"),
+    Nunito_600SemiBold: require("../../../assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+    Nunito_700Bold: require("../../../assets/fonts/Nunito/Nunito-Bold.ttf"),
+    Nunito_800ExtraBold: require("../../../assets/fonts/Nunito/Nunito-ExtraBold.ttf"),
   })
 
   return loaded
