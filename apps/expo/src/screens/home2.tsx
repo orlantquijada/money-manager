8d6ba897c01afa2701f385141d37bf8f12819e8d
Orlan Quijada <orlantquijada@gmail.com>
2023-07-22 00:07:08 +0800

    feat: shrink main header progress bar on scroll

--- apps/expo/src/screens/home2.tsx
+++ apps/expo/src/screens/home2.tsx
@@ -8,6 +8,10 @@ import { HeaderProgressBar } from "~/components/dashboard/HeaderProgressBar"
 import FoldersList from "~/components/dashboard/FoldersList"
 
 import Plus from "../../assets/icons/plus.svg"
+import {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated"
 
 export default function Home2() {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null)
@@ -15,6 +19,17 @@ export default function Home2() {
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present()
   }, [])
+
+  const didScroll = useSharedValue(0)
+  const offset = 200
+
+  const handler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      const y = Math.ceil(event.contentOffset.y)
+      if (y > offset && !didScroll.value) didScroll.value = 1
+      else if (y < offset && didScroll.value) didScroll.value = 0
+    },
+  })
 
   return (
     <SafeAreaView className="bg-violet1 flex-1">
@@ -29,9 +44,12 @@ export default function Home2() {
           </Pressable>
         </View>
 
-        <HeaderProgressBar progress={Math.random() * 100} />
+        {/* <HeaderProgressBar progress={Math.random() * 100} /> */}
+        <View className="z-10 -mb-8">
+          <HeaderProgressBar progress={90} didScroll={didScroll} />
+        </View>
 
-        <FoldersList />
+        <FoldersList onScroll={handler} />
 
         <DashboardCreateBottomSheet ref={bottomSheetModalRef} />
       </View>
