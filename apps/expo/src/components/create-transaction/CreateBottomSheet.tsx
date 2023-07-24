16c218340f23ae5065273bbb8e4e698922d48bf8
Orlan Quijada <orlantquijada@gmail.com>
2023-07-24 23:23:23 +0800

    feat(wip): inital form ui

--- apps/expo/src/components/create-transaction/CreateBottomSheet.tsx
+++ apps/expo/src/components/create-transaction/CreateBottomSheet.tsx
@@ -1,5 +1,11 @@
-import { ComponentProps, forwardRef, useMemo } from "react"
-import { Text, View } from "react-native"
+import { ComponentProps, forwardRef, useMemo, useState } from "react"
+import {
+  Text,
+  TextInput,
+  View,
+  Button as RNButton,
+  Platform,
+} from "react-native"
 import {
   BottomSheetBackdropProps,
   BottomSheetHandleProps,
@@ -13,6 +19,8 @@ import Animated, {
   SharedValue,
   useAnimatedStyle,
 } from "react-native-reanimated"
+import { format, formatRelative, subDays } from "date-fns"
+import DateTimePicker from "@react-native-community/datetimepicker"
 
 import { mauveDark } from "~/utils/colors"
 
@@ -21,8 +29,13 @@ import Button from "../Button"
 import ScaleDownPressable from "../ScaleDownPressable"
 
 import CrossIcon from "../../../assets/icons/hero-icons/x-mark.svg"
+import WalletIcon from "../../../assets/icons/wallet-duo.svg"
+import CalendarIcon from "../../../assets/icons/calendar-duo-dark.svg"
+import NoteIcon from "../../../assets/icons/note-duo-dark.svg"
 
-const snapPoints = ["25%", "94%"]
+// const snapPoints = ["25%", "94%"]
+// 184 = handle + header + payee + fund height
+const snapPoints = [184, "94%"]
 const backgroundColor = mauveDark.mauve2
 
 const TransactionCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
@@ -33,9 +46,15 @@ const TransactionCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
 
   const { dismissAll } = useBottomSheetModal()
 
+  const selectedDate = subDays(new Date(), 5)
+  const formattedDate = formatRelative(selectedDate, new Date())
+  console.log(formattedDate)
+  const [date, time] = formattedDate.split(" at ")
+
   return (
     <BottomSheet
       snapPoints={snapPoints}
+      stackBehavior="push"
       backdropComponent={CustomBackdrop}
       ref={ref}
       index={1}
@@ -49,8 +68,8 @@ const TransactionCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
         overflow: "hidden",
       }}
     >
-      <View className="flex-1 px-4">
-        <View className="flex-row items-center justify-between">
+      <View className="flex-1">
+        <View className="flex-row items-center justify-between px-4">
           <ScaleDownPressable onPress={dismissAll}>
             <CrossIcon
               color={mauveDark.mauve12}
@@ -66,11 +85,97 @@ const TransactionCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
             </Text>
           </Button>
         </View>
+
+        <View className="border-b-mauveDark4 h-16 flex-row items-center border-b px-4">
+          <View className="aspect-square w-5" />
+          <TextInput
+            className="font-satoshi-medium text-mauveDark12 ml-4 h-full grow text-xl"
+            placeholder="Store or Item"
+            placeholderTextColor={mauveDark.mauve10}
+          />
+        </View>
+
+        <View className="border-b-mauveDark4 h-16 flex-row items-center border-b px-4">
+          <WalletIcon width={20} height={20} />
+          <TextInput
+            className="font-satoshi-medium text-mauveDark12 ml-4 h-full grow"
+            style={{ fontSize: 16 }}
+            placeholder="Select Fund"
+            placeholderTextColor={mauveDark.mauve10}
+          />
+        </View>
+
+        <View className="border-b-mauveDark4 h-16 flex-row items-center border-b px-4">
+          <CalendarIcon width={20} height={20} />
+          <View className="ml-4 grow flex-row justify-between">
+            <Text className="font-satoshi-medium text-mauveDark12 text-base capitalize">
+              {date}
+            </Text>
+            <Text className="font-satoshi-medium text-mauveDark12 text-base">
+              {time || format(selectedDate, "K:mm aa")}
+            </Text>
+          </View>
+        </View>
+
+        <DateComp />
+
+        <View className="border-b-mauveDark4 h-16 flex-row items-center border-b px-4">
+          <NoteIcon width={20} height={20} />
+          <TextInput
+            className="font-satoshi-medium text-mauveDark12 ml-4 h-full grow"
+            style={{ fontSize: 16 }}
+            placeholder="Add Note"
+            placeholderTextColor={mauveDark.mauve10}
+          />
+        </View>
       </View>
     </BottomSheet>
   )
 })
 TransactionCreateBottomSheet.displayName = "TransactionCreateBottomSheet"
+
+function DateComp() {
+  const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState("date")
+  const [show, setShow] = useState(false)
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate
+    setShow(false)
+    setDate(currentDate)
+  }
+
+  const showMode = (currentMode) => {
+    setShow(true)
+    setMode(currentMode)
+  }
+
+  const showDatepicker = () => {
+    showMode("date")
+  }
+
+  const showTimepicker = () => {
+    showMode("time")
+  }
+
+  return (
+    <View className="bg-mauve1 px-4 py-8">
+      <RNButton onPress={showDatepicker} title="Show date picker!" />
+      <RNButton onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          onChange={onChange}
+          display={Platform.OS === "ios" ? "inline" : "default"}
+        />
+      )}
+    </View>
+  )
+}
 
 function CustomBackdrop(props: ComponentProps<typeof Backdrop>) {
   const { animatedIndex } = props
