e2240dc365c5a18570959caadcb0eccf19b7a565
Orlan Quijada <orlantquijada@gmail.com>
2023-07-28 18:18:51 +0800

    feat: store related apis

---
+++ packages/api/src/router/stores.ts
@@ -0,0 +1,19 @@
+import { z } from "zod"
+import { publicProcedure, router } from "../trpc"
+
+export const storesRouter = router({
+  listFromUserId: publicProcedure.input(z.string()).query(({ input, ctx }) => {
+    return ctx.prisma.store.findMany({
+      where: {
+        userId: input,
+      },
+      orderBy: {
+        name: "asc",
+      },
+      select: {
+        name: true,
+        id: true,
+      },
+    })
+  }),
+})
