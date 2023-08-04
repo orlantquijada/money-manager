c0d97a181fe445e5793301ee94c563b0c4a1257d
Orlan Quijada <orlantquijada@gmail.com>
2023-08-04 20:38:24 +0800

    feat: overspent progress bar

--- apps/expo/src/components/Category.tsx
+++ apps/expo/src/components/Category.tsx
@@ -1,12 +1,15 @@
+import { useMemo } from "react"
 import { View, Text } from "react-native"
-import { getWeeksInMonth } from "date-fns"
+import { getWeeksInMonth, getWeekOfMonth } from "date-fns"
+import clsx from "clsx"
 
-import { toCurrency } from "~/utils/functions"
+import { toCurrencyNarrow } from "~/utils/functions"
 import { Fund, TimeMode } from ".prisma/client"
 import ProgressBar from "./ProgressBar"
+import { pink } from "~/utils/colors"
 
 import Stripes from "@assets/icons/stripes-small-violet.svg"
-import { useMemo } from "react"
+import PinkStripes from "@assets/icons/stripes-pink.svg"
 
 const helperTextTimeModeMap: Record<TimeMode, string> = {
   WEEKLY: "this week",
@@ -15,12 +18,15 @@ const helperTextTimeModeMap: Record<TimeMode, string> = {
   EVENTUALLY: "",
 }
 
-const totalSpent = 150
-
 type CategoryProps = {
   fund: Fund
 }
 export default function Category({ fund }: CategoryProps) {
+  const weekOfMonth = getWeekOfMonth(new Date())
+  const overspentValue = weekOfMonth * Number(fund.budgetedAmount) - totalSpent
+  const didOverspend = overspentValue < 0
+
+  // console.log(overspentValue, didOverspend)
   return (
     <View className="py-2 px-4">
       <View className="flex-row justify-between">
@@ -29,14 +35,30 @@ export default function Category({ fund }: CategoryProps) {
         </Text>
 
         {/* different text format per target type */}
-        <Text className="font-satoshi text-mauve9 mt-1 text-xs">
+        <Text
+          className={clsx("font-satoshi text-mauve9 mt-1 text-xs")}
+          style={didOverspend ? { color: pink.pink9, opacity: 0.8 } : {}}
+        >
           {/* {`${toCurrency(Math.random() * 1000)} left ${ */}
           {/*   helperTextTimeModeMap[fund.timeMode] */}
           {/* }`.trim()} */}
-          <Text className="font-nunito">
-            {toCurrency(Number(fund.budgetedAmount) - totalSpent)}{" "}
-          </Text>
-          {`left ${helperTextTimeModeMap[fund.timeMode]}`.trim()}
+          {didOverspend ? (
+            <>
+              overspent
+              <Text className="font-nunito-semibold">
+                {" "}
+                {toCurrencyNarrow(overspentValue * -1)}{" "}
+              </Text>
+              {`${helperTextTimeModeMap[fund.timeMode]}`.trim()}
+            </>
+          ) : (
+            <>
+              <Text className="font-nunito-semibold">
+                {toCurrencyNarrow(Number(fund.budgetedAmount) - totalSpent)}{" "}
+              </Text>
+              {`left ${helperTextTimeModeMap[fund.timeMode]}`.trim()}
+            </>
+          )}
         </Text>
       </View>
 
@@ -46,21 +68,43 @@ export default function Category({ fund }: CategoryProps) {
 }
 
 function CategoryProgressBar({ fund }: { fund: Fund }) {
-  const fundProgress = useFundProgress(fund, totalSpent)
+  const [fundProgress, overspentProgress] = useFundProgress(fund, totalSpent)
+
+  if (overspentProgress) console.log(overspentProgress, fund.name)
 
   return (
     <View className="mt-2 flex-row gap-x-1">
+      {overspentProgress ? (
+        <ProgressBar
+          progress={100}
+          Stripes={
+            <View>
+              <PinkStripes />
+            </View>
+          }
+          className="flex-1"
+          color="transparent"
+          // color={pink.pink8}
+          style={{ flexGrow: overspentProgress / 100 }}
+        />
+      ) : null}
       {fundProgress.map((progress, index) => (
         <ProgressBar
           key={index + fund.id}
           progress={progress}
-          Stripes={Stripes}
+          Stripes={
+            <View className="opacity-[.15]">
+              <Stripes />
+            </View>
+          }
           className={"flex-1 rounded-full"}
         />
       ))}
     </View>
   )
 }
+
+const totalSpent = 500
 
 function useFundProgress(fund: Fund, totalSpent: number) {
   return useMemo(() => {
@@ -84,6 +128,8 @@ function useFundProgress(fund: Fund, totalSpent: number) {
       }
     }
 
-    return budgetProgress
+    const overspentProgress = (left / budgetedAmount) * 100
+
+    return [budgetProgress, overspentProgress] as const
   }, [fund, totalSpent])
 }
--- apps/expo/src/components/ProgressBar.tsx
+++ apps/expo/src/components/ProgressBar.tsx
@@ -1,13 +1,14 @@
 import clsx from "clsx"
-import { ComponentProps, FC } from "react"
-import { SvgProps } from "react-native-svg"
+import { ComponentProps, ReactNode } from "react"
 import { useDerivedValue, useSharedValue } from "react-native-reanimated"
 import { useDynamicAnimation } from "moti"
+import { ColorValue } from "react-native"
 
 import { clamp } from "~/utils/functions"
 import { transitions } from "~/utils/motion"
 
 import StyledMotiView from "./StyledMotiView"
+import { violet } from "~/utils/colors"
 
 type ProgressBarProps = {
   /*
@@ -15,13 +16,16 @@ type ProgressBarProps = {
    * NOTE: value is clamped
    */
   progress: number
-  Stripes: FC<SvgProps>
+  Stripes: ReactNode
+  color?: ColorValue
 } & ComponentProps<typeof StyledMotiView>
 
+// TODO: handle negative progress
 export default function ProgressBar({
   progress: progressProp,
   className,
   Stripes,
+  color = violet.violet6,
   ...props
 }: ProgressBarProps) {
   const fullWidth = useSharedValue(0)
@@ -40,21 +44,23 @@ export default function ProgressBar({
   return (
     <StyledMotiView
       {...props}
-      className={clsx("relative h-2 w-full overflow-hidden", className)}
+      className={clsx(
+        "relative h-2 w-full overflow-hidden rounded-full",
+        className,
+      )}
       onLayout={(e) => {
         fullWidth.value = e.nativeEvent.layout.width
         props.onLayout?.(e)
       }}
     >
-      <StyledMotiView className="absolute inset-0 opacity-[.15]">
-        <Stripes />
-      </StyledMotiView>
+      <StyledMotiView className="absolute inset-0">{Stripes}</StyledMotiView>
 
       {/* thumb */}
       <StyledMotiView
         state={state}
         transition={transitions.soft}
         className="bg-violet6 h-full rounded-full"
+        style={{ backgroundColor: color }}
       />
     </StyledMotiView>
   )
