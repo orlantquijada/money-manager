9af53ee5a4e292b97c363406237e9c749fde74d1
Orlan Quijada <orlantquijada@gmail.com>
2023-08-09 15:32:14 +0800

    feat: transactions list

--- apps/expo/src/components/TransactionsList.tsx
+++ apps/expo/src/components/TransactionsList.tsx
@@ -1,17 +1,15 @@
 import { SectionList, Text, View } from "react-native"
-import { format, isToday, isYesterday, subDays } from "date-fns"
+import { format, isToday, isYesterday } from "date-fns"
 
 import { toCurrencyNarrow } from "~/utils/functions"
+import { trpc } from "~/utils/trpc"
 
 export function TransactionsList() {
+  const transactions = useTransactionsList()
+
   return (
     <View className="relative flex-1">
       <SectionList
-        ListHeaderComponent={
-          <Text className="font-satoshi-medium text-mauve12 my-10 mb-4 text-xl">
-            History
-          </Text>
-        }
         showsVerticalScrollIndicator={false}
         renderItem={({ item, section, index }) => (
           <View
@@ -25,17 +23,17 @@ export function TransactionsList() {
             <View className="flex-grow flex-row items-center justify-between">
               <View>
                 <Text className="text-mauve12 font-satoshi-bold text-base capitalize">
-                  {item.fundName}
+                  {item.fund.name}
                 </Text>
                 {item.store ? (
                   <Text className="text-mauve11 font-satoshi text-base">
-                    {item.store}
+                    {item.store.name}
                   </Text>
                 ) : null}
               </View>
 
               <Text className="text-mauve11 font-nunito-semibold text-base">
-                {toCurrencyNarrow(item.amount)}
+                {toCurrencyNarrow(Number(item.amount))}
               </Text>
             </View>
           </View>
@@ -57,69 +55,45 @@ export function TransactionsList() {
             </Text>
           )
         }}
-        sections={[
-          {
-            data: [
-              { fundName: "Grocery", store: "PureGold", amount: 1325 },
-              { fundName: "Internet", store: "Converge", amount: 1625.5 },
-              { fundName: "Laundry", store: "", amount: 255 },
-            ],
-            title: new Date(),
-          },
-          {
-            data: [
-              {
-                fundName: "Books",
-                store: "Tress of the Emerald Sea",
-                amount: 1499,
-              },
-              { fundName: "Spotfy", amount: 499 },
-              { fundName: "Eating Out", store: "Jollibee", amount: 378 },
-            ],
-            title: subDays(new Date(), 1),
-          },
-          {
-            data: [
-              {
-                fundName: "Books",
-                store: "Tress of the Emerald Sea",
-                amount: 1499,
-              },
-              { fundName: "Spotfy", amount: 499 },
-              { fundName: "Eating Out", store: "Jollibee", amount: 378 },
-            ],
-            title: subDays(new Date(), 2),
-          },
-          {
-            data: [
-              {
-                fundName: "Books",
-                store: "Tress of the Emerald Sea",
-                amount: 1499,
-              },
-              { fundName: "Spotfy", amount: 499 },
-              { fundName: "Eating Out", store: "Jollibee", amount: 378 },
-            ],
-            title: subDays(new Date(), 5),
-          },
-          {
-            data: [
-              {
-                fundName: "Books",
-                store: "Tress of the Emerald Sea",
-                amount: 1499,
-              },
-              { fundName: "Spotfy", amount: 499 },
-              { fundName: "Eating Out", store: "Jollibee", amount: 378 },
-            ],
-            title: subDays(new Date(), 5),
-          },
-        ]}
-        // contentContainerStyle={{ paddingBottom: 48, position: "relative" }}
+        sections={transactions.data || []}
         contentContainerStyle={{
           paddingBottom: 48,
         }}
       />
     </View>
   )
+}
+
+const formatKey = (date: Date) => {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+}
+const parseKey = (dateString: string) => {
+  const [year, month, date] = dateString.split("-").map(Number)
+  // @ts-expect-error type should be Number bec of the cast
+  return new Date(year, month, date)
+}
+
+function useTransactionsList() {
+  return trpc.transaction.allThisWeek.useQuery(undefined, {
+    select: (data) => {
+      // date string as key
+      const _data: Record<string, typeof data[number][]> = {}
+
+      for (const transaction of data) {
+        const day = transaction.date
+        const key = formatKey(transaction.date || new Date())
+        if (!day) continue
+        else if (_data[key]) {
+          _data[key]?.push(transaction)
+        } else {
+          _data[key] = [transaction]
+        }
+      }
+
+      return Object.entries(_data).map(([key, value]) => ({
+        title: parseKey(key),
+        data: value,
+      }))
+    },
+  })
 }
