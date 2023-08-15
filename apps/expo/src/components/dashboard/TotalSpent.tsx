e119ca1b86d8c5593e464b1c2d4c995db0dfb879
Orlan Quijada <orlantquijada@gmail.com>
2023-08-16 00:33:35 +0800

    chore(deps): upgraded more deps

--- apps/expo/src/components/dashboard/TotalSpent.tsx
+++ apps/expo/src/components/dashboard/TotalSpent.tsx
@@ -1,13 +1,11 @@
 import { useLayoutEffect, useRef } from "react"
-import { Pressable, Text, View } from "react-native"
+import { Text, View } from "react-native"
 import Animated, {
   Layout,
   LayoutAnimationFunction,
   withDelay,
   withSpring,
 } from "react-native-reanimated"
-import * as ContextMenu from "zeego/context-menu"
-import * as DropdownMenu from "zeego/dropdown-menu"
 
 import { lime } from "~/utils/colors"
 import { toCurrencyShort } from "~/utils/functions"
@@ -15,7 +13,6 @@ import { transitions } from "~/utils/motion"
 import { trpc } from "~/utils/trpc"
 
 import ArrowDown from "../../../assets/icons/hero-icons/arrow-down.svg"
-import Button from "../Button"
 
 export default function TotalSpent() {
   const totalSpent = useTotalSpent()
@@ -61,107 +58,7 @@ export default function TotalSpent() {
       <Text className="font-satoshi-medium text-mauve10 mr-1 text-base">
         Total spent this month
       </Text>
-      <MyMenu />
-      <DropMenu />
     </View>
-  )
-}
-
-export function DropMenu() {
-  return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        <Button>
-          <Text>Hello World</Text>
-        </Button>
-      </DropdownMenu.Trigger>
-
-      <DropdownMenu.Content>
-        <DropdownMenu.Label>label 1</DropdownMenu.Label>
-        <DropdownMenu.Item key="key">
-          <DropdownMenu.ItemTitle>title 1</DropdownMenu.ItemTitle>
-        </DropdownMenu.Item>
-
-        <DropdownMenu.Group>
-          <DropdownMenu.Item key="key2">
-            <DropdownMenu.ItemTitle>title 1</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemTitle>title 2</DropdownMenu.ItemTitle>
-          </DropdownMenu.Item>
-        </DropdownMenu.Group>
-
-        <DropdownMenu.CheckboxItem key="check 2" value="mixed">
-          <DropdownMenu.ItemTitle>title 1</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIndicator />
-        </DropdownMenu.CheckboxItem>
-
-        <DropdownMenu.Sub>
-          <DropdownMenu.SubTrigger key="trigger" textValue="Hello">
-            <DropdownMenu.Item key="trigger 2">
-              <DropdownMenu.ItemTitle>title 1</DropdownMenu.ItemTitle>
-            </DropdownMenu.Item>
-          </DropdownMenu.SubTrigger>
-          <DropdownMenu.SubContent>
-            <View>
-              <Text>asd</Text>
-            </View>
-          </DropdownMenu.SubContent>
-        </DropdownMenu.Sub>
-
-        <DropdownMenu.Separator />
-        <DropdownMenu.Arrow />
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  )
-}
-
-export function MyMenu() {
-  return (
-    <ContextMenu.Root
-    // onOpenChange={(isOpen) => {
-    //   console.log(isOpen)
-    // }}
-    // onOpenWillChange={(willOpen) => {
-    //   console.log({ willOpen })
-    // }}
-    >
-      <ContextMenu.Trigger action="longPress">
-        <Button>
-          <Text>Hello</Text>
-        </Button>
-      </ContextMenu.Trigger>
-
-      <ContextMenu.Content>
-        <ContextMenu.Preview>
-          {() => <View>{/* <Text>Hello World</Text> */}</View>}
-        </ContextMenu.Preview>
-
-        <ContextMenu.Label>Label 1</ContextMenu.Label>
-        <ContextMenu.Item key="hello">
-          <ContextMenu.ItemTitle>Item Title 1</ContextMenu.ItemTitle>
-        </ContextMenu.Item>
-
-        {/* <ContextMenu.Group> */}
-        {/*   <ContextMenu.Item key="world"> */}
-        {/*     <ContextMenu.ItemTitle>Item Title 2</ContextMenu.ItemTitle> */}
-        {/*   </ContextMenu.Item> */}
-        {/* </ContextMenu.Group> */}
-
-        {/* <ContextMenu.CheckboxItem key="wow" value={false}> */}
-        {/*   <ContextMenu.ItemTitle>Item Title 3</ContextMenu.ItemTitle> */}
-        {/*   <ContextMenu.ItemIndicator /> */}
-        {/* </ContextMenu.CheckboxItem> */}
-
-        {/* <ContextMenu.Sub> */}
-        {/*   <ContextMenu.SubTrigger key="sub1"> */}
-        {/*     <Text>Hello World 2</Text> */}
-        {/*   </ContextMenu.SubTrigger> */}
-        {/*   <ContextMenu.SubContent /> */}
-        {/* </ContextMenu.Sub> */}
-
-        <ContextMenu.Separator />
-        <ContextMenu.Arrow />
-      </ContextMenu.Content>
-    </ContextMenu.Root>
   )
 }
 
