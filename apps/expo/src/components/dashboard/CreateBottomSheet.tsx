dce008729e1f76ce2b454d1e2ec2cd81e34c5e1f
Orlan Quijada <orlantquijada@gmail.com>
2023-08-21 22:45:24 +0800

    refactor: just navigate directly

--- apps/expo/src/components/dashboard/CreateBottomSheet.tsx
+++ apps/expo/src/components/dashboard/CreateBottomSheet.tsx
@@ -1,8 +1,11 @@
-import { ComponentProps, forwardRef, useRef } from "react"
+import { forwardRef } from "react"
 import { Platform, Text, View } from "react-native"
 import {
-  useBottomSheetModal,
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
   useBottomSheetSpringConfigs,
+  BottomSheetModal,
+  useBottomSheet,
 } from "@gorhom/bottom-sheet"
 
 import { mauveA, mauveDark } from "~/utils/colors"
@@ -10,7 +13,6 @@ import { useRootStackNavigation } from "~/utils/hooks/useRootStackNavigation"
 import type { RootStackParamList } from "~/types"
 import { tabbarBottomInset } from "~/navigation/TabBar"
 
-import BottomSheet, { Backdrop } from "../BottomSheet"
 import CreateCard from "./CreateCard"
 
 import FolderIcon from "../../../assets/icons/folder-closed-duo-create.svg"
@@ -30,27 +32,14 @@ type Routes = Extract<
   "CreateFund" | "CreateFolder" | "Root"
 >
 
-// HACK: navigating directly `onPress` on <CreateCard /> does not work. The bottom sheet reopens after
-// navigating - most probably has to do with rerendering since current impl works —
-// using refs to set what route to update to.
-
-const DashboardCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
-  const navigation = useRootStackNavigation()
-  const { dismissAll } = useBottomSheetModal()
-  const to = useRef<Routes>()
-
-  const handleCardOnPress = (card: Routes) => () => {
-    to.current = card
-    dismissAll()
-  }
-
+const DashboardCreateBottomSheet = forwardRef<BottomSheetModal>((_, ref) => {
   const springConfigs = useBottomSheetSpringConfigs({
     stiffness: 250,
     damping: 25,
   })
 
   return (
-    <BottomSheet
+    <BottomSheetModal
       snapPoints={snapPoints}
       animationConfigs={springConfigs}
       detached
@@ -63,65 +52,77 @@ const DashboardCreateBottomSheet = forwardRef<BottomSheet>((_, ref) => {
         overflow: "hidden",
       }}
       index={0}
+      backgroundStyle={{
+        backgroundColor: "transparent",
+      }}
       handleIndicatorStyle={{ backgroundColor: mauveDark.mauve8 }}
       handleStyle={{ backgroundColor: mauveDark.mauve1 }}
       name="dashboard-create"
-      onDismiss={() => {
-        if (to.current === undefined) return
-
-        if (to.current === "Root")
-          navigation.navigate("Root", { screen: "AddTransaction" })
-        else navigation.navigate(to.current)
-
-        to.current = undefined
-      }}
     >
-      <View className="bg-mauveDark1 border-t-mauveDark1 flex-1 border-t">
-        <View className="mb-6 flex-row items-center justify-between px-6">
-          <Text className="text-mauveDark12 font-satoshi-bold text-xl">
-            Create
-          </Text>
-
-          {/* <ScaleDownPressable className="bg-mauveDark4 h-6 w-6 items-center justify-center rounded-md"> */}
-          {/*   <CrossIcon */}
-          {/*     color={mauveDark.mauve8} */}
-          {/*     height={14} */}
-          {/*     width={14} */}
-          {/*     strokeWidth={3} */}
-          {/*   /> */}
-          {/* </ScaleDownPressable> */}
-        </View>
-        <CreateCard
-          Icon={FolderIcon}
-          title="Folder"
-          description="Add folders to organize your funds"
-          onPress={handleCardOnPress("CreateFolder")}
-        />
-        <View className="bg-mauveDark6 h-px w-full" />
-        {/* TODO: description copy */}
-        <CreateCard
-          Icon={WalletIcon}
-          title="Fund"
-          description="Description"
-          onPress={handleCardOnPress("CreateFund")}
-        />
-        <View className="bg-mauveDark6 h-px w-full" />
-        {/* TODO: description copy */}
-        <CreateCard
-          Icon={ShoppingBagIcon}
-          title="Transaction"
-          description="Description"
-          onPress={handleCardOnPress("Root")}
-        />
-      </View>
-    </BottomSheet>
+      <Content />
+    </BottomSheetModal>
   )
 })
 DashboardCreateBottomSheet.displayName = "DashboardCreateBottomSheet"
 
-function CreateBackdrop(props: ComponentProps<typeof Backdrop>) {
+function Content() {
+  const navigation = useRootStackNavigation()
+  const { close } = useBottomSheet()
+
+  // closure ni
+  const handleCardOnPress = (card: Routes) => () => {
+    close()
+
+    if (card === "Root")
+      navigation.navigate("Root", { screen: "AddTransaction" })
+    else navigation.navigate(card)
+  }
+
   return (
-    <Backdrop
+    <View className="bg-mauveDark1 border-t-mauveDark1 flex-1 border-t">
+      <View className="mb-6 flex-row items-center justify-between px-6">
+        <Text className="text-mauveDark12 font-satoshi-bold text-xl">
+          Create
+        </Text>
+
+        {/* <ScaleDownPressable className="bg-mauveDark4 h-6 w-6 items-center justify-center rounded-md"> */}
+        {/*   <CrossIcon */}
+        {/*     color={mauveDark.mauve8} */}
+        {/*     height={14} */}
+        {/*     width={14} */}
+        {/*     strokeWidth={3} */}
+        {/*   /> */}
+        {/* </ScaleDownPressable> */}
+      </View>
+      <CreateCard
+        Icon={FolderIcon}
+        title="Folder"
+        description="Add folders to organize your funds"
+        onPress={handleCardOnPress("CreateFolder")}
+      />
+      <View className="bg-mauveDark6 h-px w-full" />
+      {/* TODO: description copy */}
+      <CreateCard
+        Icon={WalletIcon}
+        title="Fund"
+        description="Description"
+        onPress={handleCardOnPress("CreateFund")}
+      />
+      <View className="bg-mauveDark6 h-px w-full" />
+      {/* TODO: description copy */}
+      <CreateCard
+        Icon={ShoppingBagIcon}
+        title="Transaction"
+        description="Description"
+        onPress={handleCardOnPress("Root")}
+      />
+    </View>
+  )
+}
+
+function CreateBackdrop(props: BottomSheetBackdropProps) {
+  return (
+    <BottomSheetBackdrop
       {...props}
       appearsOnIndex={0}
       disappearsOnIndex={-1}
@@ -130,5 +131,5 @@ function CreateBackdrop(props: ComponentProps<typeof Backdrop>) {
   )
 }
 
-export type DashboardCreateBottomSheet = BottomSheet
+export type DashboardCreateBottomSheet = BottomSheetModal
 export default DashboardCreateBottomSheet
