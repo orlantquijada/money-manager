5a96f3cf5d481afdadfbd1cdc02dbe57baa3ff91
Orlan Quijada <orlantquijada@gmail.com>
2023-08-31 16:01:53 +0800

    refactor: animate scale instead of width (perf enhancement)

--- apps/expo/src/components/fund-detail/FundDetailContent.tsx
+++ apps/expo/src/components/fund-detail/FundDetailContent.tsx
@@ -26,6 +26,8 @@ type Props = {
   fund: FundWithMeta
 }
 
+const previewScale = (width - 16 * 2) / width
+
 export default function FundDetailContent({ fund }: Props) {
   const navigation = useRootBottomTabNavigation()
   const { close } = useBottomSheet()
@@ -33,15 +35,24 @@ export default function FundDetailContent({ fund }: Props) {
   const { animatedIndex } = useBottomSheet()
 
   const style = useAnimatedStyle(() => ({
-    width: interpolate(
-      animatedIndex.value,
-      [-1, 0, 1],
-      [width - 16 * 2, width - 16 * 2, width],
-    ),
+    width,
+    transform: [
+      {
+        scale: interpolate(
+          animatedIndex.value,
+          [-1, 0, 1],
+          [previewScale, previewScale, 1],
+        ),
+      },
+    ],
   }))
-  const containerStyle = useAnimatedStyle(() => ({
+  const handleStyle = useAnimatedStyle(() => ({
     opacity: interpolate(animatedIndex.value, [-1, 0, 1], [1, 1, 0]),
-    height: interpolate(animatedIndex.value, [-1, 0, 1], [24, 24, 0]),
+  }))
+  const contentContainerStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateY: interpolate(animatedIndex.value, [-1, 0, 1], [0, 0, -24]) },
+    ],
   }))
 
   return (
@@ -51,122 +62,126 @@ export default function FundDetailContent({ fund }: Props) {
     >
       <Animated.View
         pointerEvents="none"
-        style={containerStyle}
+        style={handleStyle}
         className="h-6 items-center justify-center"
       >
         <View className="bg-mauve5 h-1 w-[27px] rounded-full" />
       </Animated.View>
 
-      <View className="mb-10 h-10 flex-row items-center justify-between pt-4">
-        <View className="flex-row items-center">
-          <View className="bg-violet4 mr-2 aspect-square w-12 items-center justify-center rounded-full">
-            <Ellipsis
-              width={24}
-              height={24}
-              strokeWidth={3}
-              color={violet.violet8}
-            />
+      <Animated.View style={contentContainerStyle}>
+        <View className="mb-10 h-10 flex-row items-center justify-between pt-4">
+          <View className="flex-row items-center">
+            <View className="bg-violet4 mr-2 aspect-square w-12 items-center justify-center rounded-full">
+              <Ellipsis
+                width={24}
+                height={24}
+                strokeWidth={3}
+                color={violet.violet8}
+              />
+            </View>
+
+            <View>
+              <Text className="text-mauve12 font-satoshi-bold text-lg">
+                {fund.name}
+              </Text>
+              <Text className="text-mauve9 font-satoshi text-sm">
+                {fundTypeReadableText[fund.fundType]}
+              </Text>
+            </View>
           </View>
-
-          <View>
-            <Text className="text-mauve12 font-satoshi-bold text-lg">
-              {fund.name}
-            </Text>
-            <Text className="text-mauve9 font-satoshi text-sm">
-              {fundTypeReadableText[fund.fundType]}
-            </Text>
-          </View>
+          <Dropdown />
         </View>
-        <Dropdown />
-      </View>
 
-      <ScaleDownPressable
-        onPress={() => {
-          close()
-          navigation.navigate("AddTransaction", { fundId: fund.id })
-        }}
-      >
-        <Button className="h-10" animate={{ backgroundColor: mauve.mauve3 }}>
-          <Text className="font-satoshi-medium text-mauve10">Add Expense</Text>
-        </Button>
-      </ScaleDownPressable>
-
-      <View className="mt-10">
-        <Text className="font-satoshi text-mauve9 text-sm">
-          <Text className="font-nunito-semibold">
-            {toCurrencyNarrow(fund.totalBudgetedAmount - fund.totalSpent)}
-          </Text>{" "}
-          Available for spending
-        </Text>
-
-        <CategoryProgressBars fund={fund} />
-      </View>
-
-      {/* details / rows */}
-      <View className="mt-8 space-y-2">
-        <View className="flex-row items-center justify-between">
-          <Text className="font-satoshi-bold text-mauve9 mr-auto text-base">
-            Total Budgeted Amount
-          </Text>
-          <Text className="font-nunito-bold text-mauve9 text-base">
-            {toCurrencyNarrow(fund.totalBudgetedAmount)}
-          </Text>
-
-          <ScaleDownPressable
-            scale={0.9}
-            className="ml-3 aspect-square w-6 items-center justify-center rounded-md"
-          ></ScaleDownPressable>
-        </View>
         <ScaleDownPressable
-          className="flex-row items-center justify-between"
-          opacity={0.6}
-          scale={1}
           onPress={() => {
             close()
-            navigation.navigate("TransactionsList", {
-              fundId: fund.id,
-              fundName: fund.name,
-            })
+            navigation.navigate("AddTransaction", { fundId: fund.id })
           }}
         >
-          <Text className="font-satoshi-medium text-mauve9 mr-auto text-base">
-            Total Spent this month
-          </Text>
-          <Text className="font-nunito-semibold text-mauve9 text-base">
-            {toCurrencyNarrow(fund.totalSpent)}
+          <Button className="h-10" animate={{ backgroundColor: mauve.mauve3 }}>
+            <Text className="font-satoshi-medium text-mauve10">
+              Add Expense
+            </Text>
+          </Button>
+        </ScaleDownPressable>
+
+        <View className="mt-10">
+          <Text className="font-satoshi text-mauve9 text-sm">
+            <Text className="font-nunito-semibold">
+              {toCurrencyNarrow(fund.totalBudgetedAmount - fund.totalSpent)}
+            </Text>{" "}
+            Available for spending
           </Text>
 
-          <View className="bg-mauve3 ml-3 aspect-square w-6 items-center justify-center rounded-md">
-            <ChevronRight
-              width={15}
-              height={15}
-              strokeWidth={3}
-              color={mauve.mauve8}
-            />
-          </View>
-        </ScaleDownPressable>
-        <ScaleDownPressable
-          className="flex-row items-center justify-between"
-          scale={1}
-          opacity={0.6}
-        >
-          <Text className="font-satoshi-medium text-mauve9 mr-auto text-base">
-            Total Spent last month
-          </Text>
-          <Text className="font-nunito-semibold text-mauve9 text-base">
-            {toCurrencyNarrow(400)}
-          </Text>
+          <CategoryProgressBars fund={fund} />
+        </View>
 
-          <View className="bg-mauve3 ml-3 aspect-square w-6 items-center justify-center rounded-md">
-            <ChevronRight
-              width={15}
-              height={15}
-              strokeWidth={3}
-              color={mauve.mauve8}
-            />
+        {/* details / rows */}
+        <View className="mt-8 space-y-2">
+          <View className="flex-row items-center justify-between">
+            <Text className="font-satoshi-bold text-mauve9 mr-auto text-base">
+              Total Budgeted Amount
+            </Text>
+            <Text className="font-nunito-bold text-mauve9 text-base">
+              {toCurrencyNarrow(fund.totalBudgetedAmount)}
+            </Text>
+
+            <ScaleDownPressable
+              scale={0.9}
+              className="ml-3 aspect-square w-6 items-center justify-center rounded-md"
+            ></ScaleDownPressable>
           </View>
-        </ScaleDownPressable>
-      </View>
+          <ScaleDownPressable
+            className="flex-row items-center justify-between"
+            opacity={0.6}
+            scale={1}
+            onPress={() => {
+              close()
+              navigation.navigate("TransactionsList", {
+                fundId: fund.id,
+                fundName: fund.name,
+              })
+            }}
+          >
+            <Text className="font-satoshi-medium text-mauve9 mr-auto text-base">
+              Total Spent this month
+            </Text>
+            <Text className="font-nunito-semibold text-mauve9 text-base">
+              {toCurrencyNarrow(fund.totalSpent)}
+            </Text>
+
+            <View className="bg-mauve3 ml-3 aspect-square w-6 items-center justify-center rounded-md">
+              <ChevronRight
+                width={15}
+                height={15}
+                strokeWidth={3}
+                color={mauve.mauve8}
+              />
+            </View>
+          </ScaleDownPressable>
+          <ScaleDownPressable
+            className="flex-row items-center justify-between"
+            scale={1}
+            opacity={0.6}
+          >
+            <Text className="font-satoshi-medium text-mauve9 mr-auto text-base">
+              Total Spent last month
+            </Text>
+            <Text className="font-nunito-semibold text-mauve9 text-base">
+              {toCurrencyNarrow(400)}
+            </Text>
+
+            <View className="bg-mauve3 ml-3 aspect-square w-6 items-center justify-center rounded-md">
+              <ChevronRight
+                width={15}
+                height={15}
+                strokeWidth={3}
+                color={mauve.mauve8}
+              />
+            </View>
+          </ScaleDownPressable>
+        </View>
+      </Animated.View>
 
       {/* <ScaleDownPressable */}
       {/*   containerStyle={{ marginTop: "auto" }} */}
