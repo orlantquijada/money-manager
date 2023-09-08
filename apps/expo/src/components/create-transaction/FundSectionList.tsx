305df09398655549ba99c2ba7c91882306b118bf
Orlan Quijada <orlantquijada@gmail.com>
2023-09-08 15:31:59 +0800

    feat: fade on fund list

--- apps/expo/src/components/create-transaction/FundSectionList.tsx
+++ apps/expo/src/components/create-transaction/FundSectionList.tsx
@@ -1,13 +1,17 @@
-import { Fund, FundType } from ".prisma/client"
-import { BottomSheetSectionList, useBottomSheet } from "@gorhom/bottom-sheet"
 import { memo } from "react"
 import { Text, View } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
+import { BottomSheetSectionList, useBottomSheet } from "@gorhom/bottom-sheet"
+import clsx from "clsx"
+
 import { fundTypeReadableText, userId } from "~/utils/constants"
 import { getTotalBudgetedAmount, toCurrencyNarrow } from "~/utils/functions"
 import { trpc } from "~/utils/trpc"
-import ScaleDownPressable from "../ScaleDownPressable"
-import clsx from "clsx"
 import { useTransactionStore } from "~/utils/hooks/useTransactionStore"
+import { mauveDark } from "~/utils/colors"
+
+import ScaleDownPressable from "../ScaleDownPressable"
+import { Fund, FundType } from ".prisma/client"
 
 // TODO: skeleton
 const FundSectionList = memo(({ searchText }: { searchText: string }) => {
@@ -34,69 +38,86 @@ const FundSectionList = memo(({ searchText }: { searchText: string }) => {
     : funds
 
   return (
-    <BottomSheetSectionList
-      sections={filteredData || []}
-      renderItem={({ item, section, index }) => {
-        const selected = item.id === selectedFund?.id
-        const moneyLeft =
-          item.fundType === "SPENDING"
-            ? item.totalBudgetedAmount - item.totalSpent * -1
-            : item.totalBudgetedAmount - item.totalSpent
+    <View className="relative flex-1">
+      <LinearGradient
+        // violet1 with 0 opacity
+        colors={["hsla(240, 5.1%, 11.6%, 0)", mauveDark.mauve2]}
+        className="absolute left-0 bottom-0 right-0 z-10 h-14"
+      />
+      <BottomSheetSectionList
+        stickySectionHeadersEnabled
+        sections={filteredData || []}
+        renderItem={({ item, section, index }) => {
+          const selected = item.id === selectedFund?.id
+          const moneyLeft =
+            item.fundType === "SPENDING"
+              ? item.totalBudgetedAmount - item.totalSpent * -1
+              : item.totalBudgetedAmount - item.totalSpent
 
-        return (
-          <View
-            className={clsx(
-              "h-12",
-              selected ? "bg-mauveDark4" : "bg-transparent",
-              section.data.length - 1 === index && "mb-6",
-            )}
-          >
-            <ScaleDownPressable
-              scale={0.98}
-              onPress={() => {
-                handleSetFund(item)
-              }}
-              className="h-full flex-row items-center justify-between self-stretch px-4"
+          return (
+            <View
+              className={clsx(
+                "h-12",
+                selected ? "bg-mauveDark4" : "bg-transparent",
+                section.data.length - 1 === index && "mb-6",
+              )}
             >
-              <Text className="text-mauveDark12 font-satoshi-medium text-base">
-                {item.name}
-              </Text>
+              <ScaleDownPressable
+                scale={0.98}
+                onPress={() => {
+                  handleSetFund(item)
+                }}
+                className="h-full flex-row items-center justify-between self-stretch px-4"
+              >
+                <Text className="text-mauveDark12 font-satoshi-medium text-base">
+                  {item.name}
+                </Text>
 
-              <View className="flex-row items-center gap-1">
-                <View
-                  className={clsx(
-                    "h-8 justify-center rounded-lg px-2",
-                    // moneyLeft > 0 && "bg-lime10",
-                    // moneyLeft < 0 && "bg-red10",
-                  )}
-                >
-                  <Text
+                <View className="flex-row items-center gap-1">
+                  <View
                     className={clsx(
-                      "font-nunito-medium text-mauveDark11 text-sm",
-                      moneyLeft > 0 && "text-limeDark10",
-                      moneyLeft < 0 && "text-redDark10",
+                      "h-8 justify-center rounded-lg px-2",
+                      // moneyLeft > 0 && "bg-lime10",
+                      // moneyLeft < 0 && "bg-red10",
                     )}
                   >
-                    {toCurrencyNarrow(moneyLeft)}
-                  </Text>
+                    <Text
+                      className={clsx(
+                        "font-nunito-medium text-mauveDark11 text-sm",
+                        moneyLeft > 0 && "text-limeDark10",
+                        moneyLeft < 0 && "text-redDark10",
+                      )}
+                    >
+                      {toCurrencyNarrow(moneyLeft)}
+                    </Text>
+                  </View>
                 </View>
+              </ScaleDownPressable>
+            </View>
+          )
+        }}
+        contentContainerStyle={{ paddingBottom: 40 }}
+        renderSectionHeader={({ section }) =>
+          section.data.length ? (
+            <View>
+              <View className="bg-mauveDark2 px-4 pb-2">
+                <Text className="text-mauveDark11 font-satoshi-medium text-sm">
+                  {fundTypeReadableText[section.title]}
+                </Text>
               </View>
-            </ScaleDownPressable>
-          </View>
-        )
-      }}
-      contentContainerStyle={{ paddingBottom: 40 }}
-      renderSectionHeader={({ section }) =>
-        section.data.length ? (
-          <View className="bg-mauveDark2 px-4 pb-2">
-            <Text className="text-mauveDark11 font-satoshi-medium text-sm">
-              {fundTypeReadableText[section.title]}
-            </Text>
-          </View>
-        ) : null
-      }
-      keyExtractor={(item) => item.id.toString()}
-    />
+              <View className="relative h-2 w-full overflow-y-visible">
+                <LinearGradient
+                  // violet1 with 0 opacity
+                  colors={[mauveDark.mauve2, "hsla(240, 5.1%, 11.6%, 0)"]}
+                  className="absolute left-0 top-0 right-0 z-10 h-6"
+                />
+              </View>
+            </View>
+          ) : null
+        }
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
   )
 })
 FundSectionList.displayName = "FundSectionList"
