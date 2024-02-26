d22ceaf8e9b6b9567d25f18d245963d3295091e3
Orlan Quijada <orlantquijada@gmail.com>
2024-02-26 20:05:50 +0800

    feat: initial vercel config

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -6,41 +6,41 @@ import { getHostIP } from "./utils"
 
 export function createServer() {
   const port = Number(process.env.PORT) || 3000
-  const server = fastify({
+  const app = fastify({
     maxParamLength: 5000,
     logger: true,
   })
 
-  server.register(clerkPlugin)
+  app.register(clerkPlugin)
 
-  server.register(fastifyTRPCPlugin, {
+  app.register(fastifyTRPCPlugin, {
     prefix: "/trpc",
     trpcOptions: { router: appRouter, createContext },
   })
 
-  server.register(fastifyTRPCPlugin, {
+  app.register(fastifyTRPCPlugin, {
     prefix: "/auth",
     trpcOptions: { router: authRouter },
   })
 
-  server.get("/ping", async () => {
+  app.get("/ping", async () => {
     return "pong"
   })
 
-  const stop = () => server.close()
+  const stop = () => app.close()
   const start = async () => {
     const host =
       process.env.NODE_ENV === "development" ? getHostIP() : "0.0.0.0"
 
     try {
-      await server.listen({ port, host })
+      await app.listen({ port, host })
       if (process.env.NODE_ENV === "development")
         console.log(`listening on ${host}:${port}`)
     } catch (err) {
-      server.log.error("err", err)
+      app.log.error("err", err)
       process.exit(1)
     }
   }
 
-  return { server, start, stop }
+  return { app, start, stop }
 }
