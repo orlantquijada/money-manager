3d09876a21f8d9074e5f9e78c70064a358d41526
Orlan Quijada <orlantquijada@gmail.com>
2024-02-26 20:05:50 +0800

    feat: initial vercel config

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,15 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
 import { createServer } from "./server"
 
-const server = createServer()
+const { app, start } = createServer()
 
-server.start()
+if (process.env.NODE_ENV === "development") {
+  start()
+}
+
+async function handler(req: FastifyRequest, res: FastifyReply) {
+  await app.ready()
+  app.server.emit("request", req, res)
+}
+
+export default handler
